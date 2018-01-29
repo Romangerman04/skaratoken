c31d30f41ecb92dac884582c9e85b03cebaf1f4e
unknown <metamoreno@gmail.com>
2018-01-29 09:28:18 +0100

    no message

--- truffle-config.js
+++ truffle-config.js
@@ -21,26 +21,23 @@ module.exports = {
       port: 8545,
       network_id: '*',
       gas: 6721975,
-      //from: "0x627306090abab3a6e1400e9345bc60c78a8bef57",
-      //from: "0x13ba42b19c25c0f6ecb7ab1c5db8d736231ecb94",
+
     },
     ropsten: {
       network_id: 3,
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/pv0QcsGlqWaGaZvEsP9i")
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/")
       },
       gas: 3000000,
-      from: "0x2e2d6a76b08f4e30a1387cb623288f53458d3c69"
     },
     
     kovan: {
       network_id: 42,
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/pv0QcsGlqWaGaZvEsP9i")
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/")
       },
       gas: 6721975,
       gasPrice: 20000000
-      //from: "0x0E56f09FDD14d61E456fbc45C618fD4FF10256e2"
     },
     coverage: {
       host: "localhost",
@@ -53,11 +50,10 @@ module.exports = {
     main: {
       network_id: 1,
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/pv0QcsGlqWaGaZvEsP9i ")
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/ ")
       },
       gas: 6721975,
       gasPrice: 10000000000
-      //from: "0x0E56f09FDD14d61E456fbc45C618fD4FF10256e2"
     },
     
   },
